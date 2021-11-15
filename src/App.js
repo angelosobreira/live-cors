e787957a91f2bfd351406588262984b849e64f7e
Angelo Sobreira <angelosobreira@gmail.com>
2021-11-15 13:04:20 -0300

    Teste Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://apl001.prosesmt.com.br:8080/api/customers")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.map((customer) => (
+          <li key={customer.id}>
+            {customer.id} - {customer.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
