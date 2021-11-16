d8387e2210b686529e2692d14ffd3626dd1ccf54
Angelo Sobreira <angelosobreira@gmail.com>
2021-11-16 11:59:45 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://api.taskip.com.br:8080/api/customers").then((res) => {
-      console.log(res.data);
-      setData(res.data);
+  async function gravaCliente() {
+    var res = await axios.put("https://api.taskip.com.br:8080/api/customers", {
+      id: 1,
+      description: "Angelo Sobreira",
     });
-  }, []);
+    console.log(res);
+  }
+
+  async function pesqCliente() {
+    var res = await axios.get("https://api.taskip.com.br:8080/api/customers");
+    console.log(res.data);
+    setData(res.data);
+  }
 
   return (
     <div>
+      <div>
+        <button onClick={pesqCliente}>Pesquisar Cliente</button>
+        <button onClick={gravaCliente}>Adicionar Cliente</button>
+      </div>
       <ul>
         {data.map((customer) => (
           <li key={customer.id}>
